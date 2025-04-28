033e1a44420144040e1a3beccdfc9acf0fb68f46
Adarsh Singh <114363643+devadarshh@users.noreply.github.com>
2025-04-28 11:38:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,44 @@
+//  use this quizData in your app.
+export const quizData = [
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Language",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+  {
+    question: "What is the primary function of HTML?",
+    a: "To add interactivity to web pages",
+    b: "To style the visual appearance of web pages",
+    c: "To structure the content of web pages",
+    d: "To manage databases for web applications",
+    correct: "c",
+  },
+  //you can add more quiz here
+];
